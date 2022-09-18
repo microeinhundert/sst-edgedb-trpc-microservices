b287646b8b068a8c61d7791e0be602dfa22e4d41
Leon Seipp <l.seipp@microeinhundert.com>
2022-09-18 13:41:44 +0200

    Enhancements

--- apps/portal/src/main.tsx
+++ apps/portal/src/main.tsx
@@ -3,7 +3,7 @@ import "./assets/app.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App";
+import { App } from "./app";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
