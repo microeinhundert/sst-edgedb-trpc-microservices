1630b8393ae9cd6f3bb085de04f7965fa42f5ec0
Leon Seipp <leon.fm100@gmail.com>
2022-11-06 20:00:28 +0100

    Fixes

--- apps/site/app/root.tsx
+++ apps/site/app/root.tsx
@@ -21,7 +21,7 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export async function loader() {
+export function loader() {
   return json({
     env: globalEnv,
   });
